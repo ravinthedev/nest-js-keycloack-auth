46453f3fcb3fdfb6f230deb68529aa421cd926af
Ravin <ravindu.lokumanna@gmail.com>
2023-09-03 12:30:35 +0530

    adding master branch

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   const env = (process.env.NODE_ENV || 'dev').trim(); // Default to 'development'
   const envFilePath = `${env}.env`;
+  console.log('test')
   // Load environment variables from the selected file
   dotenv.config({ path: envFilePath });
   const app = await NestFactory.create(AppModule);
